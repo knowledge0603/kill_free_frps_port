61a129c9e36cddd9b0e992a11ff143ae305f2a19
knowledge0603 <23024084@qq.com>
2020-11-26 20:11:36 +0800

    Update kill_free_frps_port.sh

--- kill_free_frps_port.sh
+++ kill_free_frps_port.sh
@@ -1,8 +1,5 @@
 #! /bin/bash
-#ss -atp | grep frp | grep 128| awk '{print $6}'| sed 's/^.*pid=//g'|sed 's/,.*$//g'| xargs kill -9
-result=$( ss -atp | grep frps)
-echo "---------------------------source string"
-echo "$result"
+echo "start task :`date '+%Y-%m-%d %H:%M:%S'`"
 result_listen=$(ss -apt | grep frps | grep LISTEN)
 result_estab=$(ss -apt | grep frps | grep ESTAB)
 result_listen_sed=$(ss -apt | grep frps | grep LISTEN |sed 's/^.*pid=//g'|sed 's/,.*$//g')
@@ -15,10 +12,10 @@ echo "---------------------------listen string sed"
 echo "$result_listen_sed"
 echo "---------------------------estab string sed"
 echo "$result_estab_sed"
-echo "---------------------------diff string  [kill pid] "
-#echo $(sort -m <(sort result_listen_sed.txt| uniq) <(sort result_estab_sed.txt | uniq) <(sort result_estab_sed.txt| uniq) | uniq -u)
-#echo ${result_listen_sed//result_estab_sed/---}  
+rm -f  result_listen_sed_split.txt
+rm -f  result_estab_sed_split.txt
 echo $result_listen_sed | awk '{split($0,arr," ");for(i in arr) print arr[i] }' > result_listen_sed_split.txt
 echo $result_estab_sed | awk '{split($0,arr," ");for(i in arr) print arr[i] }' > result_estab_sed_split.txt
 echo "kill pid is :" $(sort -m <(sort result_listen_sed_split.txt| uniq) <(sort result_estab_sed_split.txt | uniq) <(sort result_estab_sed_split.txt| uniq) | uniq -u)
 echo $(sort -m <(sort result_listen_sed_split.txt| uniq) <(sort result_estab_sed_split.txt | uniq) <(sort result_estab_sed_split.txt| uniq) | uniq -u) | xargs kill -9
+echo "end  task :`date '+%Y-%m-%d %H:%M:%S'`"
